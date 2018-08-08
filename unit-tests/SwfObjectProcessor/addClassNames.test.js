a29ce283a3f1311acdb29a731602e0e39325aae7
Solinca <corbi.nicolas@gmail.com>
2018-08-08 03:03:06 +0200

    Remove monkey test

--- unit-tests/SwfObjectProcessor/addClassNames.test.js
+++ unit-tests/SwfObjectProcessor/addClassNames.test.js
@@ -30,32 +30,4 @@ describe('Add class names', function () {
 
         expect(result).to.be.empty;
     });
-
-    // Monkey test
-    it('add two or more class name', function () {
-        const min = 2;
-        const max = 5;
-        const random = Math.floor(Math.random() * (max - min + 1)) + min;
-
-        const symbols = [];
-
-        const classNames = {
-            test: []
-        };
-
-        for (let i = 0; i < random; i++) {
-            symbols.push({});
-            classNames.test.push(i);
-        }
-
-        const classNamesKeys = Object.keys(classNames);
-
-        const result = addClassNames(symbols, classNames);
-
-        for (let i = 0; i < random; i++) {
-            expect(result[i]).to.exist;
-            expect(result[i]).to.have.own.property('className');
-            expect(result[i].className).to.eql(classNamesKeys[0]);
-        }
-    });
 });
